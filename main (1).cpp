842b3377d8e58ed610c2830f40da6c5472c3f22b
yno <99153168+sketchyno@users.noreply.github.com>
2023-05-21 00:37:55 +0800

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-const int maxStudCount = 1000; 
-const int maxAnnounce = 100;  
+const int maxStudCount = 1000;
+const int maxAnnounce = 100;
 
 class Student {
 public:
@@ -75,62 +75,91 @@ public:
         string title;
         string content;
 
-        cin.ignore();
-        cout << "Enter Title: ";
-        getline(cin, title);
-        cout << "Enter Content: ";
-        getline(cin, content);
+        do {
+            cin.ignore();
+            cout << "Enter Title: ";
+            getline(cin, title);
+            cout << "Enter Content: ";
+            getline(cin, content);
 
-        if (announcementCount < maxAnnounce) {
-            announcements[announcementCount].title = title;
-            announcements[announcementCount].content = content;
-            announcementCount++;
-            cout << "Announcement added successfully!" << endl;
-        }
-        else {
-            cout << "Maximum number of announcements reached. Cannot add more." << endl;
-        }
+            if (announcementCount < maxAnnounce) {
+                announcements[announcementCount].title = title;
+                announcements[announcementCount].content = content;
+                announcementCount++;
+                cout << "Announcement added successfully!" << endl;
+            } else {
+                cout << "Maximum number of announcements reached. Cannot add more." << endl;
+            }
+
+            cout << "Do you want to add another announcement? (Y/N): ";
+            char choice;
+            cin >> choice;
+
+            if (choice != 'Y' && choice != 'y')
+                break;
+
+        } while (true);
 
         pressEnterToContinue();
     }
 
     void updateAnnouncement() {
         string search;
-        cin.ignore();
-        cout << "Enter Title to Update: ";
-        getline(cin, search);
 
-        for (int i = 0; i < announcementCount; i++) {
-            if (announcements[i].title == search) {
-                cout << "Enter New Content: ";
-                getline(cin, announcements[i].content);
-                cout << "Announcement updated successfully!" << endl;
-                pressEnterToContinue();
-                return;
+        do {
+            cin.ignore();
+            cout << "Enter Title to Update: ";
+            getline(cin, search);
+
+            for (int i = 0; i < announcementCount; i++) {
+                if (announcements[i].title == search) {
+                    cout << "Enter New Content: ";
+                    getline(cin, announcements[i].content);
+                    cout << "Announcement updated successfully!" << endl;
+                    break;
+                }
             }
-        }
 
-        cout << "No matching announcement found." << endl;
+            cout << "No matching announcement found." << endl;
+            cout << "Do you want to update another announcement? (Y/N): ";
+            char choice;
+            cin >> choice;
+
+            if (choice != 'Y' && choice != 'y')
+                break;
+
+        } while (true);
+
         pressEnterToContinue();
     }
 
     void deleteAnnouncement() {
         string search;
-        cin.ignore();
-        cout << "Enter Title to Delete: ";
-        getline(cin, search);
 
-        for (int i = 0; i < announcementCount; i++) {
-            if (announcements[i].title == search) {
-                announcements[i].title = "";
-                announcements[i].content = "";
-                cout << "Announcement deleted successfully!" << endl;
-                pressEnterToContinue();
-                return;
+        do {
+            cin.ignore();
+            cout << "Enter Title to Delete: ";
+            getline(cin, search);
+
+            for (int i = 0; i < announcementCount; i++) {
+                if (announcements[i].title == search) {
+                    announcements[i].title = "";
+                    announcements[i].content = "";
+                    cout << "Announcement deleted successfully!" << endl;
+                    break;
+                }
             }
-        }
 
-        cout << "No matching announcement found." << endl;
+            cout << "No matching announcement found." << endl;
+            cout << "Do you want to delete another announcement? (Y/N): ";
+            char choice;
+            cin >> choice;
+
+            if (choice != 'Y' && choice != 'y')
+                break;
+
+        } while (true);
+
         pressEnterToContinue();
     }
 
@@ -183,27 +212,37 @@ public:
 
     void searchStudentInformation() {
         string search;
-        cin.ignore();
-        cout << "Enter SR Code to Search: ";
-        getline(cin, search);
 
-        for (int i = 0; i < studentCount; i++) {
-            if (students[i].srCode == search) {
-                cout << "Name: " << students[i].name << endl;
-                cout << "SR Code: " << students[i].srCode << endl;
-                cout << "Program: " << students[i].program << endl;
-                cout << "Section/Block: " << students[i].sectionBlock << endl;
-                if (students[i].isPaid) {
-                    cout << "Payment Status: Paid" << endl;
-                } else {
-                    cout << "Payment Status: Unpaid" << endl;
+        do {
+            cin.ignore();
+            cout << "Enter SR Code to Search: ";
+            getline(cin, search);
+
+            for (int i = 0; i < studentCount; i++) {
+                if (students[i].srCode == search) {
+                    cout << "Name: " << students[i].name << endl;
+                    cout << "SR Code: " << students[i].srCode << endl;
+                    cout << "Program: " << students[i].program << endl;
+                    cout << "Section/Block: " << students[i].sectionBlock << endl;
+                    if (students[i].isPaid) {
+                        cout << "Payment Status: Paid" << endl;
+                    } else {
+                        cout << "Payment Status: Unpaid" << endl;
+                    }
+                    break;
                 }
-                pressEnterToContinue();
-                return;
             }
-        }
 
-        cout << "No matching student found." << endl;
+            cout << "No matching student found." << endl;
+            cout << "Do you want to search for another student? (Y/N): ";
+            char choice;
+            cin >> choice;
+
+            if (choice != 'Y' && choice != 'y')
+                break;
+
+        } while (true);
+
         pressEnterToContinue();
     }
 
@@ -217,47 +256,68 @@ public:
         }
 
         Student newStudent;
-        cin.ignore();
-        cout << "===============================" << endl;
-        cout << "     Register Student    " << endl;
-        cout << "===============================" << endl;
-        cout << "Enter Name: ";
-        getline(cin, newStudent.name);
-        cout << "Enter SR Code (Username): ";
-        getline(cin, newStudent.srCode);
-        cout << "Enter Program: ";
-        getline(cin, newStudent.program);
-        cout << "Enter Section/Block: ";
-        getline(cin, newStudent.sectionBlock);
-        cout << "Enter Password: ";
-        getline(cin, newStudent.password);
 
-        students[studentCount] = newStudent;
-        studentCount++;
+        do {
+            cin.ignore();
+            cout << "===============================" << endl;
+            cout << "     Register Student    " << endl;
+            cout << "===============================" << endl;
+            cout << "Enter Name: ";
+            getline(cin, newStudent.name);
+            cout << "Enter SR Code (Username): ";
+            getline(cin, newStudent.srCode);
+            cout << "Enter Program: ";
+            getline(cin, newStudent.program);
+            cout << "Enter Section/Block: ";
+            getline(cin, newStudent.sectionBlock);
+            cout << "Enter Password: ";
+            getline(cin, newStudent.password);
 
-        cout << "===============================" << endl;
-        cout << endl;
-        cout << "~~ Student Successfully Registered ~~" << endl;
-        cout << "Please Click Enter To Exit" << endl;
-        cout << "===============================" << endl;
+            students[studentCount] = newStudent;
+            studentCount++;
+
+            cout << "===============================" << endl;
+            cout << endl;
+            cout << "~~ Student Successfully Registered ~~" << endl;
+            cout << "Please Click Enter To Exit" << endl;
+            cout << "===============================" << endl;
+            cout << "Do you want to register another student? (Y/N): ";
+            char choice;
+            cin >> choice;
+
+            if (choice != 'Y' && choice != 'y')
+                break;
+
+        } while (true);
+
         pressEnterToContinue();
     }
 
     void payFee(Student& student) {
         double cashAmount;
-        cout << "SSC Fee Payment" << endl;
-        cout << "SSC Fee: PHP 150.00" << endl;
-        cout << "Enter Cash Amount: ";
-        cin >> cashAmount;
 
-        if (cashAmount >= 150) {
-            double change = cashAmount - 150;
-            cout << change << change << endl;
-            cout << "Payment successful!  ";
-            student.isPaid = true;
-        } else {
-            cout << "Insufficient cash. Payment unsuccessful." << endl;
-        }
+        do {
+            cout << "SSC Fee Payment" << endl;
+            cout << "SSC Fee: PHP 150.00" << endl;
+            cout << "Enter Cash Amount: ";
+            cin >> cashAmount;
+
+            if (cashAmount >= 150) {
+                double change = cashAmount - 150;
+                cout << "Payment successful! Change: " << change << endl;
+                student.isPaid = true;
+            } else {
+                cout << "Insufficient cash. Payment unsuccessful." << endl;
+            }
+
+            cout << "Do you want to make another payment? (Y/N): ";
+            char choice;
+            cin >> choice;
+
+            if (choice != 'Y' && choice != 'y')
+                break;
+
+        } while (true);
 
         pressEnterToContinue();
     }
@@ -300,6 +360,7 @@ public:
 
         for (int i = 0; i < studentCount; i++) {
             if (students[i].srCode == username && students[i].password == password) {
+                currentStudentIndex = i;
                 return true;
             }
         }
@@ -332,7 +393,7 @@ public:
                     payFee(students[currentStudentIndex]);
                     break;
                 case 3:
-                    break;
+                    return;
                 default:
                     cout << "Invalid choice. Please try again." << endl;
             }
@@ -341,29 +402,18 @@ public:
         } while (studentChoice != 3);
     }
 
-public:
     void run() {
         int mainChoice;
 
         do {
-            cout << "================================" << endl;
-            cout << "Supreme Student Council" << endl;
-            cout << "Information Dissemination and Payment System" << endl;
-            cout << "================================" << endl;
-            cout << endl;
-            cout << "By" << endl;
-            cout << endl;
-            cout << "Mendoza, Janelle P." << endl;
-            cout << "Pentinio, Mhark Anthony O." << endl;
-            cout << "Saludaga, Gilbert L." << endl;
-            pressEnterToContinue();
-
-            cout << "=====================" << endl;
+            cout << "===============================" << endl;
+            cout << "         Welcome to SSC        " << endl;
+            cout << "===============================" << endl;
             cout << "1. Admin Login" << endl;
             cout << "2. Student Login" << endl;
-            cout << "3. Student Register" << endl;
+            cout << "3. Register Student" << endl;
             cout << "4. Exit" << endl;
-            cout << "=====================" << endl;
+            cout << "===============================" << endl;
             cout << "Enter your choice: ";
             cin >> mainChoice;
             cout << endl;
@@ -372,16 +422,13 @@ public:
                 case 1:
                     if (adminLogin()) {
                         adminPage();
-                    } else {
-                        cout << "Admin login unsuccessful." << endl;
-                        pressEnterToContinue();
                     }
                     break;
                 case 2:
                     if (studentLogin()) {
                         studentPage();
                     } else {
-                        cout << "Student login unsuccessful." << endl;
+                        cout << "Invalid username or password. Please try again." << endl;
                         pressEnterToContinue();
                     }
                     break;
@@ -389,8 +436,7 @@ public:
                     studentRegister();
                     break;
                 case 4:
-                    cout << "Thank you for using the SSC Information System. Goodbye!" << endl;
-                    break;
+                    return;
                 default:
                     cout << "Invalid choice. Please try again." << endl;
             }
